597f02fdcc1c76c4013d3c60fc40e672568add9d
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-02-10 14:46:36 +0100

    Update endpoint.php

--- endpoint.php
+++ endpoint.php
@@ -4,3 +4,29 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') { //Netmonster envoie uniquement des 
     header('HTTP/1.1 405 Method Not Allowed');
     exit('Method Not Allowed');
 }
+
+//Vérification si c'est un fichier JSON
+$content = file_get_contents("php://input");
+$jsonData = json_decode($content);
+
+if ($jsonData === null && json_last_error() !== JSON_ERROR_NONE) {
+    header('HTTP/1.1 400 Bad Request');
+    exit('Invalid JSON');
+}
+
+if (!isset($jsonData->author) || !isset($jsonData->date)) {
+    header('HTTP/1.1 400 Bad Request');
+    exit('JSON must contain "author" and "date" keys.');
+}
+
+// Créer le nom de fichier en utilisant les valeurs de "author" et "date"
+$filename = 'data/' . $jsonData->author . '_' . $jsonData->date . '.json';
+
+// Enregistrer le JSON dans un fichier
+$file = fopen($filename, 'w');
+fwrite($file, json_encode($jsonData, JSON_PRETTY_PRINT));
+fclose($file);
+
+// Répondre avec succès
+header('HTTP/1.1 200 OK');
+exit('Data successfully saved.');
